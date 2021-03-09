3275eafac5821cb99f24af248f8d0e3d64b30a03
Andriy <61286611+andriyrak07@users.noreply.github.com>
2021-03-09 13:12:32 +0200

    Add counter

--- script.js
+++ script.js
@@ -1,54 +1,41 @@
-let prev = document.getElementById("prev");
-let next = document.getElementById("next");
-let pos = 0;
-next.addEventListener("click", function(){
-    let slide = document.getElementById("polosa");
-    pos = pos - 700;
-    if (pos <= -2800) {
-        pos = 0;
+let slideIndex = 1;
+showSlide(slideIndex);
+
+
+function prev() {
+    showSlide(slideIndex += -1)
+}
+
+function next() {
+    showSlide(slideIndex += 1)
+}
+
+setInterval(function(){
+    showSlide(slideIndex += 1)
+}, 4000) 
+
+
+function showSlide(n) {
+    let slide = document.getElementsByTagName("img");   
+
+    if (n > slide.length) {
+        slideIndex = 1;
     }
-    slide.style.left = pos + "px";
-}, false)
 
-prev.addEventListener("click", function(){
-    let slide = document.getElementById("polosa");
-    pos = pos + 700;
-    if (pos > 0) {
-        pos = -2100;
+    if (n < 1) {
+        slideIndex = slide.length;
     }
-    slide.style.left = pos + "px";
-}, false)
 
+    for (let i = 0; i < slide.length; i++) {
+        slide[i].style.display = "none";
+    }
 
+    slide[slideIndex-1].animate([
+        {display: 'none', opacity: 0.5},
 
+        {display: 'block', opacity: 1}
+    ], 2000)
+    
 
-
-
-
-
-
-
-// let slideIndex = 1;
-// showSlide(slideIndex);
-
-// function plusSlide(n) {
-//     showSlide(slideIndex += n)
-// }
-
-// function showSlide(n) {
-//     let slide = document.getElementsByClassName("mySlide");    
-
-//     if (n > slide.length) {
-//         slideIndex = 1;
-//     }
-
-//     if (n < 1) {
-//         slideIndex = slide.length;
-//     }
-
-//     for (let i = 0; i < slide.length; i++) {
-//         slide[i].style.display = "none";
-//     }
-
-//     slide[slideIndex-1].style.display = "block";
-// }
+    slide[slideIndex-1].style.display = "block";
+}
